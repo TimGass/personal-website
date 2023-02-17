e96a150535635cd9a5d3fa06453caa5022de696f
timgass <timothyagass@gmail.com>
2023-02-17 17:14:46 -0600

    updated favicon

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <head>
         <title>Timothy Gass</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/TimGass.png" />
+        <link rel="icon" href="/favicon.ico" />
         <Head />
       </head>
       <body>

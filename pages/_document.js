71c1fe639c21dd4f5cef1d0acb5c004e5c52d472
timgass <timothyagass@gmail.com>
2023-02-17 17:25:26 -0600

    added required type field to favicon link

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <head>
         <title>Timothy Gass</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/x-icon"  href="/favicon.ico" />
         <Head />
       </head>
       <body>
